2afa4baedfdd90718d90c36547ab17a10344f031
Stephen Vizzusi <svizzusi13@gmail.com>
2023-09-24 22:51:53 -0700

    Added Navbar and footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,8 +1,29 @@
+import style from './Footer.module.css'
+import {Link} from 'react-router-dom'
+import { BsYelp } from 'react-icons/bs';
+
 const Footer = () => {
   return (
-    <>
-    
-    </>
+    <div className={style.footerContainer}>  
+      <footer className={style.footer}>
+        <span className={style.footerLogo}>Victoria's Restaurant</span>
+        <div className={style.footerAddress}>
+          <Link>757 1st St Gilroy, CA 95020</Link>
+        </div>
+        <div className={style.footerHours}>
+          <span>Mon Closed</span>
+          <span>Tue 11:00 AM - 8:00 PM</span>
+          <span>Wed 11:00 AM - 8:00 PM</span>
+          <span>Thu 11:00 AM - 8:00 PM</span>
+          <span>Fri 11:00 AM - 8:00 PM</span>
+          <span>Sat 11:00 AM - 8:00 PM</span>
+          <span>Sun Closed</span>
+        </div>
+        <div className={style.footerSocialLink}>
+          <BsYelp />
+        </div>
+      </footer>
+    </div>
   )
 };
 
--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -1,9 +1,66 @@
+import logo from '../../assets/images/logo.webp'
+import style from './NavBar.module.css'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+
 const Navbar = () => {
-  return (
-    <>
-    
-    </>
-  )
-};
+    const [navExpanded, setNavExpanded] = useState(false)
+    const navigate = useNavigate()
+
+    const openNav = () => {
+        setNavExpanded(!navExpanded)
+    }
+    const closeNav = () => setNavExpanded(false)
+
+    return (
+        <header>
+            <nav className={style.nav}>
+                <Link 
+                  to={'/'}>
+                  <img 
+                    className={style.logo} 
+                    src={logo} 
+                    alt={logo} 
+                  />
+                </Link>
+                <ul 
+                className={navExpanded ? `${style.navLinks} ${style.expanded}` : style.navLinks}
+                >
+                  <li>
+                      <span  
+                          onClick={() => { 
+                            closeNav()
+                            navigate('/') 
+                        }}
+                      >Home</span>
+                  </li>
+                  <li>
+                      <span 
+                          onClick={() => { 
+                            closeNav()
+                            navigate('/about')
+                          }}
+                      >About</span>
+                  </li>
+                  <li>
+                      <span 
+                          onClick={() => { 
+                            closeNav()
+                            navigate('/location')
+                          }}
+                      >Location & Hours</span>
+                  </li>
+                </ul>
+                <div 
+                    className={navExpanded ? `${style['icon-three']} ${style['active-three']}` : style['icon-three']}
+                    onClick={openNav}
+                >
+                    <div className={`${style['hamburger']} ${style['hamburger-three']}`}></div>
+                </div>
+            </nav>
+        </header>
+    )
+}
 
 export default Navbar;
