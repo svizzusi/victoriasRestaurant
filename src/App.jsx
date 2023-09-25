df89fa1fb696bd8e4d1098e7b8bccf2dbcf32b4f
Stephen Vizzusi <svizzusi13@gmail.com>
2023-09-24 21:47:29 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./layout/navbar/Navbar"
+import Footer from "./layout/footer/Footer"
+import Home from "./views/Home"
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/home/HeroSection.jsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+  return (
+    <>
+    
+    </>
+  )
+};
+
+export default HeroSection;
