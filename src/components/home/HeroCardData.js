f1d5ea3d802b6d752b5580340dd372d2a13abfc4
Stephen Vizzusi <svizzusi13@gmail.com>
2023-11-01 22:09:29 -0700

    Edited spelling on card

--- src/components/home/HeroCardData.js
+++ src/components/home/HeroCardData.js
@@ -1,75 +1,75 @@
-import potatoTacos from '../../assets/images/potatoTacos.webp'
-import enchiladaTaco from '../../assets/images/enchiladaTaco.webp'
-import kidsTaco from '../../assets/images/kidsTaco.webp'
-import taquitos from '../../assets/images/taquitos.webp'
-import chickenChiminanga from '../../assets/images/chickenChiminanga.webp'
-import chileColorado from '../../assets/images/chileColorado.webp'
-import fajitaSaladPlate from '../../assets/images/fajitaSaladPlate.webp'
-import johnPeralesSalad from '../../assets/images/johnPeralesSalad.webp'
+import potatoTacos from '../../assets/images/potatoTacos.webp';
+import enchiladaTaco from '../../assets/images/enchiladaTaco.webp';
+import kidsTaco from '../../assets/images/kidsTaco.webp';
+import taquitos from '../../assets/images/taquitos.webp';
+import chickenChimichanga from '../../assets/images/chickenChimichanga.webp'; // Corrected spelling
+import chileColorado from '../../assets/images/chileColorado.webp';
+import fajitaSaladPlate from '../../assets/images/fajitaSaladPlate.webp';
+import johnPeralesSalad from '../../assets/images/johnPeralesSalad.webp';
 
 export default [
     {
         id: 1,
-        img:potatoTacos,
-        title:"Potato Tacos",
-        details:"Potato Tacos are exclusively available from Wed-Sat. You have the choice of ordering them à la carte as a pair of two tacos or as a satisfying combo with Rice and Beans.",
-        price:"14.99",
+        img: potatoTacos,
+        title: "Potato Tacos",
+        details: "Potato Tacos are exclusively available from Wed-Sat. You have the choice of ordering them à la carte as a pair of two tacos or as a satisfying combo with Rice and Beans.",
+        price: "14.99",
         dataAOS: 'fade-up'
     },
     {
         id: 2,
-        img:enchiladaTaco,
-        title:"#5 Combo",
-        details:"Celebrate flavor with our Enchilada/Taco Combo, featuring a fusion of enchiladas, tacos, and a side of rice and beans. A fiesta on your plate!",
-        price:"14.99",
+        img: enchiladaTaco,
+        title: "#5 Combo",
+        details: "Celebrate flavor with our Enchilada/Taco Combo, featuring a fusion of enchiladas, tacos, and a side of rice and beans. A fiesta on your plate!",
+        price: "14.99",
         dataAOS: 'fade-up'
     },
     {
         id: 3,
-        img:kidsTaco,
-        title:"Kids Taco",
-        details:"Kids or adults with a light appitite can savor a single ground beef taco, served with a side of rice and beans. A perfect bite-sized treat for small appetites!",
-        price:"10.99",
+        img: kidsTaco,
+        title: "Kids Taco",
+        details: "Kids or adults with a light appetite can savor a single ground beef taco, served with a side of rice and beans. A perfect bite-sized treat for small appetites!",
+        price: "10.99",
         dataAOS: 'fade-up'
     },
     {
         id: 4,
-        img:taquitos,
-        title:"#9 Combo",
-        details:"Indulge in a trio of Chicken or Beef Taquitos, served alongside a flavorful portion of rice and beans. A savory delight awaits and never disappoints!",
-        price:"13.99",
+        img: taquitos,
+        title: "#9 Combo",
+        details: "Indulge in a trio of Chicken or Beef Taquitos, served alongside a flavorful portion of rice and beans. A savory delight awaits and never disappoints!",
+        price: "13.99",
         dataAOS: 'fade-up'
     },
     {
         id: 5,
-        img:chickenChiminanga,
-        title:"#18 Chiminanga",
-        details:"Our Chiminanga comes with your choice of chicken, shredded beef, or chile verde. Paired with Rice, Beans and pico de gallo",
-        price:"14.95",
+        img: chickenChimichanga,
+        title: "#18 Chimichanga",
+        details: "Our Chimichanga comes with your choice of chicken, shredded beef, or chile verde. Paired with Rice, Beans, and pico de gallo",
+        price: "14.95",
         dataAOS: 'fade-up'
     },
     {
         id: 6,
-        img:chileColorado,
-        title:"#13 Chile Colorado",
-        details:"Our #13 Chile Colorado dish is filled with cheese and your choice of meat. Served with tortillas, rice and beans",
-        price:"13.95",
+        img: chileColorado,
+        title: "#13 Chile Colorado",
+        details: "Our #13 Chile Colorado dish is filled with cheese and your choice of meat. Served with tortillas, rice, and beans",
+        price: "13.95",
         dataAOS: 'fade-up'
     },
     {
         id: 7,
-        img:fajitaSaladPlate,
-        title:"Fajita Salad Plate",
-        details:"Our mouth watering fajitas salad plate with your choice of meat. Paired with Rice, Beans and pico de gallo",
-        price:"13.95",
+        img: fajitaSaladPlate,
+        title: "Fajita Salad Plate",
+        details: "Our mouthwatering fajita salad plate with your choice of meat. Paired with Rice, Beans, and pico de gallo",
+        price: "13.95",
         dataAOS: 'fade-up'
     },
     {
         id: 8,
-        img:johnPeralesSalad,
-        title:"John Perales Salad",
-        details:"This amazing salad comes loaded with prawns, grilled chicken and special sauce. Served with rice and beans",
-        price:"17.95",
+        img: johnPeralesSalad,
+        title: "John Perales Salad",
+        details: "This amazing salad comes loaded with prawns, grilled chicken, and special sauce. Served with rice and beans",
+        price: "17.95",
         dataAOS: 'fade-up'
     }
-]
+];
